017f36118463e3a6e86f382d21261d15f38d8d2a
Neket27 <nekit.im@yandex.ru>
2022-06-02 18:44:23 +1000

    separation join

--- js/main.js
+++ js/main.js
@@ -6,17 +6,18 @@
 
 function addBlocksContent(listInfo) {
     let main = document.getElementById("table-content");
-
-    listInfo.map(data => {
-        const str =
-            '<tr> ' +
-            '<td class="main-table__field">' + data.person.firstname + " " + data.person.lastname + '</td> ' +
-            '<td class="main-table__field">' + data.car.manufacturer + '</td> ' +
-            '<td class="main-table__field">' + data.car.model + '</td> ' +
-            '<td class="main-table__field">' + data.car.year + '</td> ' +
-            '</tr>'
-        return main.innerHTML += str;
+    let rows = listInfo.map(data => {
+        const str = `
+                <tr>
+                <td class="main-table__field">${data.person.firstname} ${data.person.lastname}</td>
+                <td class="main-table__field">${data.car.manufacturer}</td>
+                <td class="main-table__field">${data.car.model}</td>
+                <td class="main-table__field">${data.car.year}</td>
+                <tr>
+                   `
+        return str;
 
     });
+    main.innerHTML += rows.join('');
 }
 
