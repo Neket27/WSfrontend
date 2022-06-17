c9db0a9bd348206637fa2cf334afef74e56bfab6
Neket27 <nekit.im@yandex.ru>
2022-06-17 16:28:21 +1000

    .

--- javascript/tasks4.js
+++ javascript/tasks4.js
@@ -1,9 +1,12 @@
-// import moment from 'moment';
+const moment = require('moment');
 
 const today = new Date();
 window.console.log(today);
 const stringDate = today.toLocaleDateString();
+
+     
 window.console.log(stringDate);
+
 const tomorrow = moment()
     .add(1, 'days')
     .format('DD-MM-yyyy');
